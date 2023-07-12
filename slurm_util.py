052d797f74080e7f87a382bee548aae243bc3b8d
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2023-07-12 18:02:21 -0500

    add job_list class to slurm_util.py

--- slurm_util.py
+++ slurm_util.py
@@ -9,49 +9,114 @@ shawn.s.murdzek@noaa.gov
 #---------------------------------------------------------------------------------------------------
 
 import os
+import numpy as np
+import pandas as pd
+
+
+#---------------------------------------------------------------------------------------------------
+# Job List Class
+#---------------------------------------------------------------------------------------------------
+
+class job_list:
+    
+    def __init__(self, fname=None, df=None, jobs=[]):
+        if fname != None:
+            self.df = pd.read_csv(fname)
+        elif df != None:
+            self.df = df
+        else:
+            nrow = len(jobs)
+            tmp = {'file':jobs, 'submitted':[False]*nrow, 'completed':[False]*nrow, 
+                   'jobID':np.zeros(nrow)*np.nan, 'tries':np.zeros(nrow, dtype=int)}
+            self.df = pd.DataFrame(tmp)
+
+
+    def save(self, save_fname):
+        self.df.to_csv(save_fname, index=False)
+        return None
+
+
+    def update(self, user, maxtries):
+        """
+        Updates job_list DataFrame about active and completed jobs from Slurm
+
+        Parameters 
+        ----------
+        user : string
+            User ID
+        maxtries : integer
+            Maximum number of attempts for each job
+
+        """
+
+        idx = np.where(np.logical_and(self.df['submitted'], ~self.df['completed']))[0]
+        for i in idx:
+            jobID = self.df.loc[i, 'jobID']
+            sacct_out = os.popen('sacct --jobs=%d' % jobID).read().split('\n')[2].split()
+            if sacct_out[5] == 'COMPLETED':
+                if sacct_out[6] == '0:0':
+                    self.df.loc[i, 'completed'] = True
+                else:
+                    if self.df.loc[i, 'tries'] < maxtries:
+                        self.df.loc[i, 'submitted'] = False
+
+        return None
+
+
+    def submit_jobs(self, user, max_jobs, verbose=True):
+        """
+        Submit jobs until all jobs are submitted or the max number of active jobs is reached
+
+        Parameters
+        ----------
+        user : string
+            User ID
+        max_jobs : integer
+            Maximum number of active jobs
+        verbose : boolean, optional
+            Print the name of each of the jobs being submitted
+
+        """
+
+        njobs = job_number(user)
+        if verbose:
+            print('initial njobs = %d' % njobs)
+            print()
+
+        idx = np.where(~self.df['submitted'])
+        for i in range(min(max_jobs - njobs, len(idx))):
+            self.df['jobID'] = int(os.popen('sbatch %s' % self.df['file'].iloc[i]).read().strip().split(' ')[-1])
+            self.df['tries'].iloc[i] = self.df['tries'].iloc[i] + 1
+            self.df['submitted'].iloc[i] = True
+            if verbose:
+                print('submitted job = %d' % self.df['jobID'])
+
+        return None        
+
 
 #---------------------------------------------------------------------------------------------------
 # Functions
 #---------------------------------------------------------------------------------------------------
 
-def job_info(job_df, user, maxtries):
+def job_number(user):
     """
-    Returns information about active and completed jobs from Slurm
+    Returns the number of active jobs
 
     Parameters 
     ----------
-    job_df : pd.DataFrame
-        DataFrame containing job information
     user : string
         User ID
-    maxtries : integer
-        Maximum number of attempts for each job
 
     Returns
     -------
-    job_df : pd.DataFrame
-        DataFrame containing updated job information
     njobs : integer
         Number jobs queued or running 
 
     """
 
-    # Determine number of queued/running jobs
     njobs = len(os.popen('squeue -u %s' % user).read().split('\n')) - 2
 
-    # Determine job statuses and mark jobs as either completed or need to be resubmitted
-    idx = np.where(np.logical_and(job_df['submitted'], ~job_df['completed']))[0]
-    for i in idx:
-        jobID = job_df.loc[i, 'jobID']
-        sacct_out = os.popen('sacct --jobs=%d' % jobID).read().split('\n')[2].split()
-        if sacct_out[5] == 'COMPLETED':
-            if sacct_out[6] == '0:0':
-                job_df.loc[i, 'completed'] = True
-            else:
-                if job_df.loc[i, 'tries'] < maxtries:
-                    job_df.loc[i, 'submitted'] = False
-
-    return job_df, njobs
+    return njobs
 
 
 """
