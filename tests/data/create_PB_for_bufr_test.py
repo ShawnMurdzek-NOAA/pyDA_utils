7f0bcfa8437327e576cf6295613e452cda9f0cef
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-09-18 22:14:40 +0000

    Add BUFR test data

--- tests/data/create_PB_for_bufr_test.py
+++ tests/data/create_PB_for_bufr_test.py
@@ -16,7 +16,7 @@ import numpy as np
 # Input Parameters
 #---------------------------------------------------------------------------------------------------
 
-in_fname = '/mnt/lfs4/BMC/wrfruc/murdzek/real_obs/decoded_bufr_for_cloudDA/202202011200.rap.prepbufr.csv'
+in_fname = '/mnt/lfs5/BMC/wrfruc/murdzek/real_obs/decoded_bufr_for_cloudDA/202202011200.rap.prepbufr.csv'
 out_fname = './202202011200.rap.prepbufr.for_bufr_test.csv'
 
 
@@ -26,13 +26,17 @@ out_fname = './202202011200.rap.prepbufr.for_bufr_test.csv'
 
 in_bufr = bufr.bufrCSV(in_fname)
 
-# Keep all type 187 obs
+# Keep all type 187 obs (never SYNOP according to PB table 5)
 keep_idx = np.where(np.isclose(in_bufr.df['TYP'], 187))[0]
 
-# Keep first 20 SID for other obs
-all_types = np.unique(in_bufr.df['TYP'])
+# Keep first 20 SID for other unrestricted obs
+# NOTE: Various mesonet and SYNOP obs are restricted. Aircraft obs are only restricted <48 hrs 
+# after collection
+keep_types = [120, 126, 130, 131, 133, 134, 135, 153, 
+              220, 224, 227, 230, 231, 233, 234, 235, 242, 243, 250, 252, 253, 254]
+all_types = np.unique(in_bufr.df['TYP'].values)
 for t in all_types:
-    if ~np.isclose(t, 187):
+    if t in keep_types:
         keep_idx = np.concatenate([keep_idx, np.where(np.isclose(in_bufr.df['TYP'], t))[0][:20]])
 
 out_df = in_bufr.df.iloc[keep_idx, :].copy()
